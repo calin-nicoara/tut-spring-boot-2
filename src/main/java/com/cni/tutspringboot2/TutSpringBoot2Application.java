236ee90a02fac999ef708fe9968a1bc785035826
Calin Nicoara <calin.nicoara@esolutions.ro>
2017-12-28 15:03:49 +0200

    First commit.

---
+++ src/main/java/com/cni/tutspringboot2/TutSpringBoot2Application.java
@@ -0,0 +1,12 @@
+package com.cni.tutspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutSpringBoot2Application.class, args);
+	}
+}
